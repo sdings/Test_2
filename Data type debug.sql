c5cac3b585c6fba8ebdd119d2f6de001a31cb012
steveporz <46386106+steveporz@users.noreply.github.com>
2019-01-07 13:35:47 -0500

    Update Data type debug.sql

--- Data type debug.sql
+++ Data type debug.sql
@@ -1,19 +1,23 @@
 select distinct Net_Payment from RawFiles
 where Net_Payment not like '%[0-9]%';
 
+--returns non-numeric Net Payment amounts
+
 select distinct Net_Payment
 from RawFiles
 where try_Cast (replace(net_Payment,',','') as float) is null
 and Net_Payment is not null;
 
+--returns Net Payment values that cause an error when converting to float
+
 select distinct Quantity from Rawfiles
 where Quantity not like '%[0-9]%';
+
+--returns non-numeric Quantity amounts
 
 select distinct Quantity
 from RawFiles
 where try_Cast (replace(Quantity,',','') as float) is null
 and Quantity is not null;
 
-select * from RawFiles
-where Net_Payment like '%E%'
-or Quantity like '%E%';
+--returns Quantity values that cause an error when converting to float
